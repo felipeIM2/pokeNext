efcd56595a68cbc7deb105f0c79cfd9b71e835b5
Luis Moreira <luisf.infu@gmail.com>
2022-11-16 19:06:39 -0300

    repair

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { useState } from "react";
 
+
 export async function getServerSideProps(context) {
   const pokemons = await axios.get(
-    "https://pokeapi.co/api/v2/pokemon?limit=1000"
+    "https://pokeapi.co/api/v2/pokemon?limit=950"
   );
 
   return {
@@ -18,41 +19,43 @@ export default function Index(props) {
   const [dex, setDex] = useState(props.pokemons.results);
   let search = props.search.pokemon;
   
-  let nameLower = []
-  if(search != undefined) {
-     const lower = search.toLowerCase()
-      nameLower.push(lower)
-  }
+  //Verificação
 
-  
-  // //verificação
-
-  // let pokeNames = dex.map((value) => value.name);
+   let pokeNames = dex.map((value) => value.name);
   // let verify = pokeNames.find((e) => e == search);
   // if (search != undefined && verify == undefined) {
   // }
 
-  //trazer skins
+  //Revisão da pesquisa
+  let nameLower = []
+  if(search != undefined) {
+    const lower = search.toLowerCase();
+    nameLower.push(lower)
+  }
+
+  //Procura da skin do pokemon
   dex.map((value) => {
     if (value.name == nameLower) {
-      async function skn() {
+      async function call() {
         await axios
           .get(value.url)
           .then((data) => (
             setSkin(data.data.sprites.front_default)
           ));
       }
-      skn();
+      call();
     }
   });
   let [skin, setSkin] = useState([]);
+
+	
 
   return (
     <>
       <div className="bg-blue-700 min-h-screen max-h-full">
         <div className="mx-auto w-[400px]">
-          <div className="absolute top-[50px] h-[50px] h-[400px] w-[400px] bg-red-600 shadow-black shadow-2xl rounded-[10px]">
-            <h1 className="absolute left-[150px] text-[25px] top-[8px]">
+          <div className="absolute top-[50px] h-[50px] h-[400px] w-[440px] bg-red-600 shadow-black shadow-2xl rounded-[10px]">
+            <h1 className="absolute left-[160px] text-[20px] top-[35px]">
               <strong>Pokedéx</strong>
             </h1>
             <div className=" h-[45px] flex bg-red-600 mt-[10px]">
@@ -68,11 +71,11 @@ export default function Index(props) {
             {/* Pokedex exibir: */}
             <div className="text-center w-[220px] h-[210px] px-[20px] mx-auto mt-[15px] ml-[90px] p-[10px] bg-slate-300 shadow shadow-black rounded-bl-[20px]">
               {/* Estilos Exibir lado de cima */}
-              <div className="flex w-[35px] mx-auto space-x-[15px]">
+              <div className="flex w-[35px] mx-auto space-x-[15px] ">
                 <span className="shadow shadow-black w-[10px] h-[10px] rounded-[30px] bg-red-700 flex"></span>
                 <span className="shadow shadow-black w-[10px] h-[10px] rounded-[30px] bg-red-700 flex"></span>
               </div>
-              <div className="shadow shadow-black h-[140px] bg-gray-800 mt-[5px] rounded-[10px] p-[2px]">
+              <div className="shadow shadow-black h-[150px] bg-gray-800 mt-[5px] rounded-[10px] p-[2px]">
                 <div className="text-white">
                   {dex.map((value) => {
                     if (nameLower == value.name) {
@@ -86,8 +89,8 @@ export default function Index(props) {
                               <div className="flex flex-col items-center w-[180px] h-[180px] mx-auto ml-[4px]">
                                 <img
                                   src={skin}
-                                  width="120px"
-                                  height="120px"
+                                  width="110px"
+                                  height="110px"
                                   className="absolute bottom-[165px]"
                                 />
                               </div>
@@ -114,7 +117,7 @@ export default function Index(props) {
             </div>
             <div className="">
               <div className="flex flex-col">
-                <div className="mt-[10px] mb-[5px] flex">
+                <div className="mt-[10px] mb-[5px] mr-[55px] flex">
                   <span className=" w-[40px] h-[10px] mx-auto flex bg-red-700 rounded-[30px] mr-[10px] shadow shadow-black"></span>
                   <span className=" w-[40px] h-[10px] mx-auto flex bg-blue-700 rounded-[30px] ml-[10px] shadow shadow-black"></span>
                 </div>
@@ -133,9 +136,21 @@ export default function Index(props) {
                     />
                   </form>
                   <div>
+                    <select name="pokemon" className="absolute bottom-[310px] right-[20px] w-[100px] text-center outline-none rounded-[5px] bg-red-600">
+                      <option disabled="on" selected="on" className="text-slate-300">Pokelist</option>
+                      {dex.map(value => {
+                        return (
+                          <option className="border-none " disabled="" key={value.url}>
+                          {value.name}
+                          </option>
+                        )
+                      })}
+                    </select>
+                  </div>
+                  <div>
                     <div className="w-[90px] h-[90px] absolute right-[25px] bottom-[20px]">
-                      <div className="border-[10px] border-gray-700 w-[90px] absolute bottom-[35px] rounded-[30px] "></div>
-                      <div className="border-[10px] border-gray-700 h-[90px] absolute right-[34px] rounded-[30px] "></div>
+                      <div className="border-[10px] border-gray-700 w-[90px] absolute bottom-[35px] right-[8px] rounded-[30px] "></div>
+                      <div className="border-[10px] border-gray-700 h-[90px] absolute right-[44px] rounded-[30px] "></div>
                     </div>
                   </div>
                 </div>
